eb5931fbf5d2a16b5800a513d9d792ecf890a34b
Rewinder18 <unclegran18@gmail.com>
2023-03-26 19:15:12 +0600

    Enabled validation and added class library

--- Diplomka/Pages/LogAndRegPage.cshtml.cs
+++ Diplomka/Pages/LogAndRegPage.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Diplomka.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace Diplomka.Pages
 {
@@ -33,9 +34,7 @@ namespace Diplomka.Pages
         }
         public IActionResult OnPostLog(string loginLog, string psswdLog) 
         {
-
             var auth = _context.Authentications.FirstOrDefault(a => a.LoginName == loginLog && a.Password == psswdLog);
-
             if (auth != null) 
             {
                 string roleId = _context.Users.FirstOrDefault(u => u.AuthenticationId == auth.Id).RoleId;
@@ -52,8 +51,45 @@ namespace Diplomka.Pages
         public async Task<IActionResult> OnPostReg(string nameReg, string surnameReg, string emailReg, string loginReg, string psswdReg, string rpsswdReg) 
         {
             var auth = _context.Authentications.FirstOrDefault(a => a.LoginName == loginReg);
+            if (string.IsNullOrEmpty(psswdReg))
+            {
+                Console.WriteLine("Xyi ty che tut proverish");
+                return Page();
+            }
             if (psswdReg.Equals(rpsswdReg))
             {
+                string patternIdIncoming = @"{\d{1}}";
+                Regex regex = new Regex(patternIdIncoming);
+                Match match = regex.Match(nameReg);
+                if (string.IsNullOrEmpty(nameReg) || match.Success)
+                {
+                    return Page();
+                }
+                match = regex.Match(surnameReg);
+                if (string.IsNullOrEmpty(surnameReg)|| match.Success)
+                {
+                    Console.WriteLine("хуйня твоя фамилия, переделывай");
+                    return Page();
+                }
+                patternIdIncoming = @"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])";
+                regex = new Regex(patternIdIncoming);
+                match = regex.Match(emailReg);
+                if (string.IsNullOrEmpty(emailReg) || !match.Success)
+                {
+                    Console.WriteLine("я тебе блять не почтовый голубь, нормально имэйл пиши");
+                    return Page();
+                }
+                if (string.IsNullOrEmpty(loginReg))
+                {
+                    Console.WriteLine("ты долбаеб хуйня логин");
+                    return Page();
+                }
+                if(string.IsNullOrEmpty(psswdReg))
+                {
+                    Console.WriteLine("минимум 4 символа, еблан");
+                    return Page();
+                }
+                    //красава авторизуем тебя
                 if (auth == null)
                 {
                     string authenId = Guid.NewGuid().ToString().ToUpper();
@@ -77,6 +113,7 @@ namespace Diplomka.Pages
                     isErrExistUser = true;
                     return Page();
                 }
+               
             }
             else
             {
